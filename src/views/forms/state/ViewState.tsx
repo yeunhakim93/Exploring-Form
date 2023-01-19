84c9f73e1c60090e74954574dab13f7767612879
Yeuna Kim <yeunhakim93@gmail.com>
2023-01-18 16:58:57 -0800

    is unadjacencify even a word?

--- src/views/forms/state/ViewState.tsx
+++ src/views/forms/state/ViewState.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useFormBuilder } from "../../../redux/slices/formBuilder/useFormBuilder";
-import { unAdjecencify } from "../../../redux/slices/formBuilder";
+import { unAdjacencify } from "../../../redux/slices/formBuilder";
 
 const ViewState: React.FC = () => {
   const { formData } = useFormBuilder();
@@ -21,7 +21,7 @@ const ViewState: React.FC = () => {
     >
       {isAdjacencyList && <pre>{JSON.stringify(formData, null, 2)}</pre>}
       {!isAdjacencyList && (
-        <pre>{JSON.stringify(unAdjecencify(formData), null, 2)}</pre>
+        <pre>{JSON.stringify(unAdjacencify(formData), null, 2)}</pre>
       )}
       <button
         onClick={(e) => {
